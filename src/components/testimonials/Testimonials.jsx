9ac9b482e24ff1c9832de941cdb74755c33e8730
asiimwearnold <asiimwearnold25@gmail.com>
2021-09-07 13:37:39 +0300

    first commit

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,64 @@
+import "./testimonials.scss";
+
+export default function Testimonials() {
+  const data = [
+    {
+      id: 1,
+      name: "Simon Kalange Mafabi",
+      title: "Co-Founder of Skylux",
+      img:
+        "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/twitter.png",
+      desc:
+        "An awesome software engineer, dedicated to his work, very amazing personality and outgoing character.",
+    },
+    {
+      id: 2,
+      name: "JP",
+      title: "Senior Software Engineer",
+      img:
+        "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/youtube.png",
+      desc:
+        "Cool guy, very creative with alot of imaginative ideas. takes time to listen and reach out, Good friend. ",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Martin Harold",
+      title: "CEO of ALBI",
+      img:
+        "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/linkedin.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
+  return (
+    <div className="testimonials" id="testimonials">
+      <h1>Testimonials</h1>
+      <div className="container">
+        {data.map((d) => (
+          <div className={d.featured ? "card featured" : "card"}>
+            <div className="top">
+              <img src="assets/right-arrow.png" className="left" alt="" />
+              <img
+                className="user"
+                src={d.img}
+                alt=""
+              />
+              <img className="right" src={d.icon} alt="" />
+            </div>
+            <div className="center">
+              {d.desc}
+            </div>
+            <div className="bottom">
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
