522a3e4dad7681c7822058d4135f38079bd11a9b
Arnold <39159396+asiimwearnold@users.noreply.github.com>
2021-11-16 05:39:24 +0300

    Update App.jsx
    Fix: un used var

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro"
-import Portfolio from "./components/portfolio/Portfolio"
-import Works from "./components/works/Works"
-import Testimonials from "./components/testimonials/Testimonials"
-import Contact from "./components/contact/Contact"
+//import Portfolio from "./components/portfolio/Portfolio"
+//import Works from "./components/works/Works"
+i//mport Testimonials from "./components/testimonials/Testimonials"
+//import Contact from "./components/contact/Contact"
 import "./app.scss"
 import { useState } from "react";
 import Menu from "./components/menu/Menu";
