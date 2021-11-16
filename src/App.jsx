92b8820396d39ac177e93593f9070008c48dbee6
Arnold <39159396+asiimwearnold@users.noreply.github.com>
2021-11-16 05:47:25 +0300

    Update App.jsx
    Fix: import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro"
+import Intro from "./components/intro/Intro";
 //import Portfolio from "./components/portfolio/Portfolio"
 //import Works from "./components/works/Works"
-i//mport Testimonials from "./components/testimonials/Testimonials"
+//mport Testimonials from "./components/testimonials/Testimonials"
 //import Contact from "./components/contact/Contact"
-import "./app.scss"
+import "./app.scss";
 import { useState } from "react";
 import Menu from "./components/menu/Menu";
 
